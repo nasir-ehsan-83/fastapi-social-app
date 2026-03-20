7d7a0898c138845ddeda3ff0da32bc9e2fe36694
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-20 17:56:00 +0430

    feat: add User class to model users information as a table in the database

---
+++ backend/app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+from backend.app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True, nullable = False)
+    email = Column(String, nullable = False, unique = True)
+    password = Column(String, nullable = False)
+    created_at = Column(TIMESTAMP(timezone = True), server_default = text("now()"), nullable = False)
