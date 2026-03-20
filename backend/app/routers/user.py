36a37908a8d1a938a9dcf018212961552afbb604
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-20 23:31:02 +0430

    feat: add post method in routers/user.py to request to  create new user

---
+++ backend/app/routers/user.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.orm import Session
+
+from backend.app.db.database import get_db
+from backend.app.schemas.user import UserCreate, UserOut
+from backend.app.services.user_service import create_user
+
+router = APIRouter(
+    prefix = '/users',
+    tags = ["Users"]
+)
+
+@router.post('/', response_model = UserOut)
+def create_new_user(user_in: UserCreate, db: Session = Depends(get_db)):
+    # call create_user() from user_service.py to performe operations in user_service
+    return create_user(user_in, db)
