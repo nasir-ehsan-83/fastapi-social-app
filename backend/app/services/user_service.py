0590f4b7700cddcf13182c5e7dfc9b1184ec504d
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-20 23:34:19 +0430

    feat: add create_user() in user_service.py to hash the password before storing user in the database

---
+++ backend/app/services/user_service.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+from backend.app.schemas.user import UserCreate
+from backend.app.crud.user_crud import add_new_user
+from backend.app.core.security import hash
+
+def create_user(user_in: UserCreate, db: Session):
+    # hash the user_in.password before storing in the database
+    hashed_password = hash(user_in.password)
+    user_in.password = hashed_password
+
+    return add_new_user(user_in, db)
