127c2691f62187fc49faae78c885aad9882534e0
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-24 23:21:08 +0430

    feat: transfer the all user_crud operations into user_service.py

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 
 from app.schemas.user import UserCreate
 from app.models.user import User
-from app.crud.user_crud import add_new_user, get_user_with_email
 from app.core.security import hash
 
 def create_user(user_in: UserCreate, db: Session) -> User:
@@ -11,11 +10,20 @@ def create_user(user_in: UserCreate, db: Session) -> User:
     hashed_password = hash(user_in.password)
     user_in.password = hashed_password
 
-    return add_new_user(user_in, db)
+    new_user = User(**user_in)
+    # add user into database
+    db.add(new_user)
+    # commit the changes
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
 def get_user_by_email(email: str, db: Session) -> User:
-    user = get_user_with_email(email, db)
+    # find the user from database
+    user = db.query(User).filter(User.email == email).first()
 
+    # if the user by specific email does not exist return an exception
     if not user:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"User with email: {email} does not exist!")
     
