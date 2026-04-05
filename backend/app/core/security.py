f0b8a012c41cb46e86ff600de6641665641bb55b
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-05 23:20:24 +0430

    fix(hash, verify): fix the Asynchronize logic by using run_in_threadpool()

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,15 +1,20 @@
 from passlib.context import CryptContext
+from fastapi.concurrency import run_in_threadpool
 
-password_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash(password: str) -> str:
+async def hash(password: str) -> str:
+    # bcrypt has a 72-byte limit; truncating safely
     password_bytes = password.encode("utf-8")[:72]
-    password_truncade = password_bytes.decode("utf-8", errors = "ingore")
 
-    return password_context.hash(password_truncade)
+    password_truncated = password_bytes.decode("utf-8", errors="ignore")
 
-def verify(plain_password: str, hashed_password: str) -> bool:
+    # Offload the heavy hashing to a threadpool
+    return await run_in_threadpool(password_context.hash, password_truncated)
+
+async def verify(plain_password: str, hashed_password: str) -> bool:
     if not hashed_password:
         return False
     
-    return password_context.verify(plain_password, hashed_password)
+    # Offload the heavy verification to a threadpool
+    return await run_in_threadpool(password_context.verify, plain_password, hashed_password)
