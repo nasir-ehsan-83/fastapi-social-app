e45f59bf4da92958d77a3f6e29657a69aeadc0cf
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-05 23:08:12 +0430

    feat: Implement JWT logic in core/oauth2.py and autherization logic (get_current_user) in dependency/current_user.py

---
+++ backend/app/core/oauth2.py
@@ -0,0 +1,48 @@
+from fastapi.concurrency import run_in_threadpool
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from typing import Dict
+
+from app.schemas.token import TokenData
+from app.core.config import settings
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+async def create_access_token(data: Dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    # Use run_in_threadpool for the CPU-bound encoding
+    encoded_jwt = await run_in_threadpool(
+        jwt.encode, 
+        to_encode, 
+        SECRET_KEY, 
+        algorithm=ALGORITHM
+    )
+    
+    return encoded_jwt
+
+async def verify_access_token(token: str, credentials_exception):
+    try: 
+        # Use run_in_threadpool for the CPU-bound decoding
+        payload = await run_in_threadpool(
+            jwt.decode, 
+            token, 
+            SECRET_KEY, 
+            algorithms=[ALGORITHM]
+        )
+
+        user_id: str = payload.get("user_id")
+
+        if user_id is None:
+            raise credentials_exception
+        
+        token_data = TokenData(id=str(user_id))
+
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
