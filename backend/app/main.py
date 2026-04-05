00694e830d068b39bf89a3e7b35fcb1dc5293b34
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-05 23:22:45 +0430

    feat: add auth router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.db import database
 from fastapi_offline_docs.offline_docs import setup_offline_docs
-from app.routers import user, post
+from app.routers import user, post, auth
 
 
 app = FastAPI(docs_url=False, redoc_url=False)
@@ -11,6 +11,7 @@ setup_offline_docs(app)
 async def init_tables():
     async with database.engine.begin() as conn:
         await conn.run_sync(database.Base.metadata.create_all)
-
+        
+app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(post.router)
