96923e8df0e2c70efce76c407d98ab42f1979add
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-06 23:55:21 +0430

    fix(login): change the spell of satuse_code to status_code in HTTPException()

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -4,16 +4,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import select
 
 from app.models.user import User
-from app.core.security import verify # to verfiry is the password hashed
+from app.core.security import hash, verify # to verfiry is the password hashed
 from app.core.oauth2 import create_access_token
 
 async def login(user_credential: OAuth2PasswordRequestForm, db: AsyncSession):
+    print(user_credential.password, user_credential.username)
     
     user_query = await db.execute(select(User).filter(User.username == user_credential.username))
     user = user_query.scalars().first()
 
     if user is None:
-        raise HTTPException(satuse_code = status.HTTP_404_NOT_FOUND, detail = "Invalid credential")
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Invalid credential")
     
     if not await verify(user_credential.password, user.password):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Invalid credential")
