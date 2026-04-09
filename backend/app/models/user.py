ae890b71f4568c8dd0b5784413ff1767d5d04c9e
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-09 18:18:23 +0430

    refactor: remove unneccesary import (relationship, ForeignKey)

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
