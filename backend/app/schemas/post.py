eb230c0d627ecde804bf401d3c18270827926299
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-14 13:40:26 +0430

    refactor: add more fields for PostBase, PostOut, PostUpdate

--- backend/app/schemas/post.py
+++ backend/app/schemas/post.py
@@ -7,7 +7,10 @@ from app.schemas.user import UserOut
 class PostBase(BaseModel):
     title: str = Field(min_length = 3, max_length = 50)
     content: str = Field(max_length = 250)
-    published: bool
+    post_status: str
+    post_type: str
+    post_visibility: str
+    post_media_url: str
 
 class PostCreate(PostBase):
     pass
@@ -15,6 +18,7 @@ class PostCreate(PostBase):
 class PostOut(PostBase):
     id: int
     created_at : datetime
+    updated_at: datetime
     owner: UserOut
 
     model_config = ConfigDict(from_attributes = True)
@@ -22,4 +26,8 @@ class PostOut(PostBase):
 class PostUpdate(BaseModel):
     title: Optional[str] = Field(default = None, min_length = 3, max_length = 50)
     content: Optional[str] = Field(default = None, max_length = 250)
-    published: Optional[bool] = None
+    post_status: Optional[str] = None
+    post_type: Optional[str] = None
+    post_visibility: Optional[str] = None
+    post_media_url: Optional[str] = None
+    updated_at: datetime = Field(default_factory = datetime.utcnow)
