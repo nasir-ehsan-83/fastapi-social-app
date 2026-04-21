02d75e6576f8ea778335298b6938d5598a8b1a9f
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-21 22:01:24 +0430

    refactor(user-response): update response schema for different access in routers/user.py

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession # تغییر به AsyncSession
-from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession 
+from typing import List, Optional
 
 from app.db.database import get_db
 from app.models.user import User
 from app.dependency.current_user import get_current_user
-from app.schemas.user import UserCreate, UserOut, UserUpdate
+from app.schemas.user import UserCreate, UserAdminOut, UserPrivateOut, UserPublicOut, UserUpdate
 from app.services.user_service import (
     create_user, get_user_by_email, update_user_by_id, 
     get_user_by_username, get_user_by_id, delete_user_by_id, get_all_users,
@@ -18,40 +18,43 @@ router = APIRouter(
 )
 
 # post user
-@router.post('/', response_model=UserOut)
+@router.post('/', response_model=UserPrivateOut)
 async def create_new_user(user_in: UserCreate, db: AsyncSession = Depends(get_db)):
     
     return await create_user(user_in, db)
 
-
 # get all user
-@router.get('/', response_model=List[UserOut])
+@router.get('/', response_model=List[UserAdminOut])
 async def get_all_user(db: AsyncSession = Depends(get_db)) -> List[User]:
     
     return await get_all_users(db)
 
-# get user by email
-@router.get('/email/{email}', response_model=UserOut)
-async def get_user_email(email: str, db: AsyncSession = Depends(get_db)):
-    return await get_user_by_email(email, db)
+# get user by email or username
+@router.get('/lookup', response_model = UserPrivateOut)
+async def get_user_email(email: Optional[str] = None, username: Optional[str] = None, db: AsyncSession = Depends(get_db)):
+    if email:
+        return await get_user_by_email(email, db)
 
-# get user by username
-@router.get('/username/{username}', response_model=UserOut)
-async def get_user_username(username: str, db: AsyncSession = Depends(get_db)):
-    return await get_user_by_username(username, db)
+    if username:
+        return await get_user_by_username(username, db)
+    
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail = "Provide email or username"
+    )
 
 # get user by name
-@router.get('/name/{name}', response_model = List[UserOut])
+@router.get('/search', response_model = List[UserPublicOut])
 async def get_user_name(name: str, db: AsyncSession = Depends(get_db)) -> List[User]:
     return await get_users_by_name(name, db)
 
 # get user by id
-@router.get('/id/{id}', response_model=UserOut)
+@router.get('/id/{id}', response_model=UserAdminOut)
 async def get_user_id(id: int, db: AsyncSession = Depends(get_db)):
     return await get_user_by_id(id, db)
 
 # update user by id
-@router.put('/id', response_model=UserOut)
+@router.put('/id', response_model=UserPrivateOut)
 async def update_user( updated_user: UserUpdate, current_user: int = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
     return await update_user_by_id(current_user, updated_user, db)
 
