09cacd4c83b27956c268f6f3a69ce785b25da9a5
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-22 20:41:36 +0430

    feat(post-schema): update Post Response  schema: add PostAdminOut schema for admin response. add PostPrivateOut schema for post owner response. add PostPublicOut schema for other users response.

--- backend/app/schemas/post.py
+++ backend/app/schemas/post.py
@@ -1,33 +1,50 @@
 from pydantic import BaseModel, ConfigDict, Field, field_validator
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional
 
-from app.schemas.user import UserOut
+from app.schemas.user import UserAdminOut, UserPrivateOut, UserPublicOut
 
 class PostBase(BaseModel):
     title: str = Field(min_length = 3, max_length = 50)
     content: str = Field(max_length = 250)
-    status: str
+
+class PostCreate(PostBase):
     type: str
     visibility: str
     media_url: str
 
-class PostCreate(PostBase):
-    pass
-
-class PostOut(PostBase):
+class PostAdminOut(PostBase):
     id: int
-    created_at : datetime
+    type: str
+    status: str
+    visibility: str
+    media_url: str
+    owner_id: int
+
+    created_at: datetime
     updated_at: datetime
-    owner: UserOut
+    
+    owner: UserAdminOut
+
+    model_config = ConfigDict(from_attributes = True)
+
+class PostPrivateOut(PostBase):  
+    id: int  
+    visibility: str
+    created_at: datetime
+    
+    onwer: UserPrivateOut
+
+    model_config = ConfigDict(from_attributes = True)
+
+class PostPublicOut(PostBase):
+    owner: UserPublicOut
+    pass
 
     model_config = ConfigDict(from_attributes = True)
 
 class PostUpdate(BaseModel):
     title: Optional[str] = Field(default = None, min_length = 3, max_length = 50)
     content: Optional[str] = Field(default = None, max_length = 250)
-    status: Optional[str] = None
-    type: Optional[str] = None
     visibility: Optional[str] = None
     media_url: Optional[str] = None
-    updated_at: Optional[datetime] = Field(default = datetime.utcnow)
