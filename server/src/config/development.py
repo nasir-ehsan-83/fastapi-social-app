26914ff5ab58ed1840777abf6806e3803a92ae36
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-08-21 21:56:18 +0430

    feat(configuration): remove default values in DevelopmentSettings and update import directory

--- server/src/config/development.py
+++ server/src/config/development.py
@@ -1,14 +1,14 @@
-from .config import Settings
+from src.config import ConfigSettings
 from pydantic_settings import SettingsConfigDict
 
-class DevelopmentSettings(Settings):
+class DevelopmentSettings(ConfigSettings):
     DEBUG: bool
-    LOG_LEVEL: str = "DEBUG"
+    LOG_LEVEL: str
     
-    model_config = SettingsConfigDict(
-        env_file=(".env", ".env.development"),
-        env_file_encoding="utf-8",
-        extra="ignore"
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_file = (".env", ".env.development"),
+        env_file_encoding = "utf-8",
+        extra = "ignore"
     )
 
-dev_settings = DevelopmentSettings()
+dev_settings: DevelopmentSettings = DevelopmentSettings() # type: ignore
--- server/src/config/production.py
+++ server/src/config/production.py
@@ -5,10 +5,10 @@ class ProductionSettings(ConfigSettings):
     DEBUG: bool
     LOG_LEVEL: str
     
-    model_config = SettingsConfigDict(
-        env_file=(".env", ".env.production"),
-        env_file_encoding="utf-8",
-        extra="ignore"
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_file = (".env", ".env.production"),
+        env_file_encoding = "utf-8",
+        extra = "ignore"
     )
 
 prod_settings: ProductionSettings = ProductionSettings() # type: ignore
